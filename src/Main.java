18b94bafe76798e113f8112edc706a381f484d2a
NikitaIlinov <makar13g@gmail.com>
2022-11-01 20:31:24 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -17,9 +18,18 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
+        Stream<Person> firstStream = persons.stream();
+        long count = firstStream.filter(w -> w.getAge() < 18).count();
 
-        Stream<Person> minor = persons.stream();
-        persons.stream()
-                .
+        Stream<Person> secondStream = persons.stream();
+        List<String> surnames = secondStream
+                .filter(w -> w.getAge() >= 18 && w.getAge() <= 27 && w.getSex() == Sex.MAN)
+                .map(Person::getFamily).collect(Collectors.toList());
+
+        Stream<Person> thirdStream = persons.stream();
+        List<Person> workers = thirdStream
+                .filter(w -> w.getAge() >= 18 && w.getAge() <= (w.getSex() != Sex.MAN ? 60 : 65) && w.getEducation() == Education.HIGHER)
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
     }
 }
